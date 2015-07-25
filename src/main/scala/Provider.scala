0fcb6a8daabbd7f735c5ba81dceff6543a874ed5
Alexa DeWit <alexa.dewit@gmail.com>
2015-07-25 03:24:39 +0200

    Adds a provider trait

---
+++ src/main/scala/Provider.scala
@@ -0,0 +1,5 @@
+package com.github.alexadewit.scala_oauth2
+
+import scalaz._, scalaz.syntax.either._, scalaz.concurrent._
+
+trait Provider
---
+++ src/test/scala/ProviderSpec.scala
@@ -0,0 +1,6 @@
+import com.github.alexadewit.scala_oauth2._
+import org.scalatest._
+
+class ProviderSpec extends FlatSpec {
+}
+
