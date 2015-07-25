7d9e59e6b6dc66df06cdef8ae9a489fe76c6d1ea
Alexa DeWit <alexa.dewit@gmail.com>
2015-07-25 03:52:01 +0200

    TYPE CLASSES WAT

---
+++ src/main/scala/providers/Google.scala
@@ -0,0 +1,9 @@
+package com.github.alexadewit.scala_oauth2.providers
+
+object Google extends Provider {
+  
+  /* I think it works like this...? Typeclasses!
+  implicit object requestBuilder extends AccessTokenRequestBuilder[Google] {
+  }
+  */
+}
