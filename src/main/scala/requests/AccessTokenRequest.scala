d0acafa67b92dfdfc154d8b74e2e20629645baa7
Alexa DeWit <alexa.dewit@gmail.com>
2015-07-25 03:43:48 +0200

    Adds the token task function

--- src/main/scala/requests/AccessTokenRequest.scala
+++ src/main/scala/requests/AccessTokenRequest.scala
@@ -5,15 +5,37 @@ import scalaz._, scalaz.syntax.either._, scalaz.concurrent._
 
 import org.http4s._
 import org.http4s.Http4s._
+import org.http4s.client._
 import org.http4s.util._
+import org.http4s.Status.NotFound
+import org.http4s.Status.ResponseClass.Successful
 
-trait AccessTokenRequest[Provider] {
+import AccessTokenJson._
+
+trait AccessTokenRequestBuilder[Provider] {
 
   def build(
     requestUri: Uri,
     authCode: String,
     keys: OAuth2Keys,
     host: String ) : Task[Request]
+
+}
+
+object AccessTokenRequest {
+
+  def accessToken(
+    request: Task[Request],
+    client: Client = org.http4s.client.blaze.defaultClient
+  ) : Task[ String \/ AccessToken ] = {
+
+    client( request ).flatMap {
+      case Successful( jsonResp ) => jsonResp.as[AccessToken].map( _.right )
+      case NotFound( resp )       => Task.now("404 Not Found".left)
+      case resp                   => Task.now(s"Failed: ${resp}".left)
+    }
+
+  }
 }
 
 object AccessTokenRequestUtils {
