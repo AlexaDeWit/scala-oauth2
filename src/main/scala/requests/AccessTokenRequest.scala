5097c46aec33f3824fe6447763178e733d7bc43c
Alexa DeWit <alexa.dewit@gmail.com>
2015-07-25 03:25:12 +0200

    Adds an access Token request trait

---
+++ src/main/scala/requests/AccessTokenRequest.scala
@@ -0,0 +1,23 @@
+package com.github.alexadewit.scala_oauth2
+
+import org.apache.commons.codec.binary.Base64
+import scalaz._, scalaz.syntax.either._, scalaz.concurrent._
+
+import org.http4s._
+import org.http4s.Http4s._
+import org.http4s.util._
+
+trait AccessTokenRequest[Provider] {
+
+  def base64EncodedKeys( keys: OAuth2Keys, delimiter: String = ":" ) : String = {
+    Base64.encodeBase64String (
+      s"${keys.clientId}${delimiter}${keys.secretKey}".getBytes()
+    )
+  }
+
+  def build(
+    requestUri: Uri,
+    authCode: String,
+    keys: OAuth2Keys,
+    host: String ) : Task[Request]
+}
---
+++ src/test/scala/requests/AccessTokenRequestSpec.scala
@@ -0,0 +1,6 @@
+import com.github.alexadewit.scala_oauth2._
+import org.scalatest._
+
+class AccessTokenRequestSpec extends FlatSpec {
+}
+
