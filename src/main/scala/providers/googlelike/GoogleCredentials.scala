8ade584924fccb002ffe60c3ee684a88e412ba19
Alexa DeWit <alexa.dewit@gmail.com>
2015-08-05 18:52:49 +0200

    Removes format specific methods no longer required

--- src/main/scala/providers/googlelike/GoogleCredentials.scala
+++ src/main/scala/providers/googlelike/GoogleCredentials.scala
@@ -4,10 +4,5 @@ import com.github.alexadewit.scala_oauth2.OAuth2Keys
 import org.http4s._, Http4s._
 
 case class GoogleCredentials( keys: OAuth2Keys, redirectUri: Uri ) {
-
-  def headerPart : Headers = Headers.empty
-  def bodyPart: Map[String, Seq[String]] = {
-    Map( "redirect_uri" -> Seq(redirectUri.toString) )
-  }
 }
 
